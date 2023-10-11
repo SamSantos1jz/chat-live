25dbc804f521e6703c18fd7ec4cba14266249a95
SamSantos1jz <samsantos1jz@gmail.com>
2023-10-11 16:07:42 -0300

    feat: criando popup

--- chat.py
+++ chat.py
@@ -2,11 +2,23 @@ import flet as ft
 
 
 def main(page):
-    text_title = ft.Text("ChatTESTE")
+    text_title = ft.Text("Chat Ao Vivo")
     
+    user_name = ft.TextField()    
+
+    popup = ft.AlertDialog(
+        open=False,
+        modal=True,
+        title=ft.Text("Bem vindo ao chat ao vivo"),
+        content=user_name,
+        actions=[ft.ElevatedButton("Entrar")]
+    )
+
     def init_chat(event):
-        text_enter = ft.Text("Entrou no CHAT")
-        page.add(text_enter)
+        page.dialog = popup
+        popup.open = True
+        page.update()
+        
 
     button_init = ft.ElevatedButton("Iniciar CHAT", on_click=init_chat)
     
